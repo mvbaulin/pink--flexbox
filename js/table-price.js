eb8c413544be8df8804b84c6d9909b832b2ce39c
Maksim <mvbaulin@yandex.ru>
2019-07-01 17:05:00 +0300

    added price-list

---
+++ js/table-price.js
@@ -0,0 +1,44 @@
+let priceTable = document.querySelector('.section-price__table');
+let priceTableDots = document.querySelectorAll('.section-price__dot');
+let priceTableNeedWidthToScroll = 87.5;
+
+function priceTableNoJs () {
+	if (priceTable.classList.contains('section-price__table--nojs')) {
+		priceTable.classList.remove('section-price__table--nojs')
+	}
+}
+
+console.log(priceTableNeedWidthToScroll);
+
+let priceTableCurrentPostion = 0;
+let priceTableCurrentDot = 0;
+
+for (let i = 0; i < priceTableDots.length; i++) {
+	priceTableDots[i].addEventListener('click', function() {
+		priceTableCurrentDot = i;
+		priceTableCurrentPostion = - priceTableNeedWidthToScroll * i;
+		priceTableDots.forEach(function(item, index, array) {
+			item.classList.remove('slider-dot--active');
+			array[priceTableCurrentDot]. classList.add('slider-dot--active');
+			priceTable.style.transform = "translateX(" + priceTableCurrentPostion + "vw)";
+		})
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+priceTableNoJs();
